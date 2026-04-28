c4db3b42c7107fb91897bbeae12f04a55354d4d9
Rakshit-Chadgal <subservience0@gmail.com>
2026-04-29 01:08:00 +0530

    Printing any string multiple times, forward and reverse

---
+++ Printing a str multiple times.py
@@ -0,0 +1,17 @@
+#Q1: Print "Hello" 5 times using a while loop.
+'''count = 1
+while count<=5:
+    print("Hello")
+    count+=1'''
+
+#Q2: Print numbers from 1 to 100 using a while loop.
+'''num = 1
+while num<=100:
+    print(num)
+    num+=1'''
+
+#Q3: Print numbers from 100 to 1 using a while loop.
+'''num = 100
+while num>=1:
+    print(num)
+    num-=1'''
