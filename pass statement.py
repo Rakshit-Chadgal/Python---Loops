f6214b2bae55139afa4b8419a768c3fa017a1aee
Rakshit-Chadgal <subservience0@gmail.com>
2026-04-29 19:44:50 +0530

    Initial Commit

---
+++ pass statement.py
@@ -0,0 +1,16 @@
+#pass statement helps us to run an empty loop or function without any error. It is used as a placeholder for future code.
+#Q1: WAP to find the sum of first n numbers. (using while loop)
+'''n = int(input("Enter a number: "))
+sum = 0
+i=1
+while i<=n:
+    sum+=i
+    i+=1
+print("The sum of first",n,"numbers is:",sum)'''
+
+#Q2: WAP to find the factorial of a number. (using for loop)
+'''n = int(input("Enter a number: "))
+factorial = 1
+for i in range(1, n+1):
+    factorial *= i
+print("The factorial of",n,"is:",factorial)'''
