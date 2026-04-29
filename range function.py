014352d71263ba7a07c8123d920b846ec0e22c3f
Rakshit-Chadgal <subservience0@gmail.com>
2026-04-29 19:31:34 +0530

    Initial Commit

---
+++ range function.py
@@ -0,0 +1,12 @@
+#Q1: Print numbers from 1 to 100.
+'''i = 1
+for i in range(1, 101):
+    print(i)
+for i in range(100, 0, -1): #Q2: Print numbers from 100 to 1.
+    print(i)'''
+
+#Q3: Print the multiplication table of a number 'n' entered by the user.
+'''n = int(input("Enter a number: "))
+i=1
+for i in range(1, 11):
+    print(f"{n} x {i} = {n*i}")'''
