2738fb87477c8b1f48029b0f8d9c03c35b40ec7e
Rakshit-Chadgal <subservience0@gmail.com>
2026-04-29 15:29:58 +0530

    Final

--- Printing multiple items using loop.py
+++ Printing multiple items using loop.py
@@ -37,4 +37,20 @@ while i<len(tup):
 if not found:
     print(x, "is not found in the tuple.")'''
 
-#Q6
+#Q6: By using for loop, print the elements of the list: [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+'''list = [1, 4, 9, 16, 25, 36, 49, 64, 81,100]
+i = 0
+for i in list:
+    print(i)
+    i+=1'''
+
+#Q7: By using loop, search for a number x in this tuple: [1, 4, 9, 16, 25, 36, 49, 64, 81,100]
+'''tup = (1, 4, 9, 16, 25, 36, 49, 64, 81,100)
+x = int(input("Enter a number to search: "))
+i = 0
+for i in tup:
+    print(i)
+    if i == x:
+        print(x, "is found in the tuple.")
+        i+=1
+        break'''
